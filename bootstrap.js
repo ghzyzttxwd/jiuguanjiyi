0abeaa14a42f4a97ae75afa7f8571916b413bc5c
ghzyzttxwd <2697941746@qq.com>
2026-09-08 18:30:41 +0800

    Load v0.8 history compactor

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-// Variable Archive Bridge v0.7.0 bootstrap
-// Universal MVU archive + recall + hot-state governance + warm catalog + safe auto cooling + self-update.
+// Variable Archive Bridge v0.8.0 bootstrap
+// Universal MVU archive + recall + hot-state governance + warm catalog + safe object/history cooling + self-update.
 
 import './macro_mux.js';
 import './index.js';
@@ -9,6 +9,7 @@ import './smart_scan.js';
 import './hot_state_governor.js';
 import './warm_catalog.js';
 import './safe_cooling_executor.js';
+import './history_compaction_executor.js';
 import './auto_cooling_scheduler.js';
 import './unified_runtime.js';
 import './self_update.js';
